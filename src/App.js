844f178022d38434b2950f4eecb7166dfdd69f14
MahmoudElshahat <melthkeel@gmail.com>
2022-11-13 12:45:14 +0200

    React-labs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AdoptedPetContext from './contexts/AdoptedPetContext';
+import Loader from './components/Loader';
+// import "./App.css"
 
-function App() {
+
+const Details = lazy(() => import('./pages/Details'));
+const SearchParams = lazy(() => import('./pages/SearchParams'));
+
+
+const NotFound = () => <h1>Page Not Found</h1>;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+       
+          <Suspense
+            fallback={
+              <div className="loader-container">
+                <Loader />
+              </div>
+            }
+          >
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
+    </BrowserRouter>
   );
-}
+};
+
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
 
 export default App;
